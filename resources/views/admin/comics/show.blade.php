6c2967592c358f229fe036a9f54311452a05b061
GiadaMarzapane <giada.ortesta@gmail.com>
2023-03-28 17:10:22 +0200

    Push view show

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.admin')
 
 @section('content')
-    
+    <h1>
+        {{ $comic->name }}
+    </h1>
+
+    <p>
+        Prezzo: € {{ $comic->price }}
+    </p>
+
+    <p>
+        {{ $comic->description }}
+    </p>
+
+    <div>
+        <img src="{{ $comic->image }}" alt="">
+    </div>
+
+    <p>
+        Quantità disponibili: {{ $comic->quantity }}
+    </p>
+
+    <div class="mb-5">
+         <a href="" class="btn btn-warning my-1">
+            Aggiorna
+        </a>
+        <a href="" class="btn btn-danger">
+            Elimina
+        </a>
+    </div>
+
 @endsection
