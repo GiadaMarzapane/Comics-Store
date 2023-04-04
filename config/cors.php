a2d844543f487063454d6ea0ea178c1f9b2a76d2
GiadaMarzapane <giada.ortesta@gmail.com>
2023-04-04 19:00:57 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('API_DEFAULT_URL', 'http://localhost:5174')],
 
     'allowed_origins_patterns' => [],
 
